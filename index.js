27fe1339649c207a272dd4a7fddbe8e15f7b54e2
Andy Arias <146486695+andyarias30@users.noreply.github.com>
2023-10-05 16:54:42 -0400

    nammed objects

---
+++ index.js
@@ -0,0 +1,20 @@
+const person= { // decalre a new object
+  firstName: "Todd",
+  age: 28
+}
+// print the person name 
+console.log(person.firstName)
+ // add one to the person name 
+ person.age++ 
+ console.log(person.age)
+
+ person.lastName = "Albert" // add new property to the object
+ /* 
+ const person = {
+    firstName: "Todd"
+    age: 29
+    lastName: "Albert"
+ }
+ */
+
+ console.log(person)
